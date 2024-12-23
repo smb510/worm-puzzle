004e39ea284957371fb38403078e781a40a9f581
Scottie Biddle <smb510@gmail.com>
2024-12-23 09:36:33 -0800

    Story for page 4

--- app/four/page.tsx
+++ app/four/page.tsx
@@ -1,39 +1,58 @@
 "use client";
 
 
+import OnCompleteHeader from '../components/OnCompleteHeader';
 import Header from '../components/Header';
-import { Box, Typography, Stack, Button, Link } from '@mui/material';
+import { Box, Typography, Stack, Button, Link, TextField } from '@mui/material';
 import { useState } from 'react';
+import { Phase } from '../components/States';
 
 export default function PageFour() {
-
-    const [isMatch, setMatch] = useState(false)
-
+    const [isCorrect, setCorrect] = useState(false)
+    const [phase, setPhase] = useState(Phase.Story)
     return <div>
-    <Header answer="decorations" question="What is the spangram?" onMatch={(b) => setMatch(b)}/>
-    {!isMatch &&
-    <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center">
-        <iframe src='https://strandstudio.thegoodboi.net/play?id=3FAR9S' height={700} />
-    </Box>
-    }
-        {isMatch &&
+        <OnCompleteHeader title="Chapter Three: Everyone is Hungry" isComplete={false} button={false} onComplete={(_: boolean) => { }} />
         <Box
             display="flex"
             justifyContent="center"
             alignItems="center">
-            <Stack direction="column">
-                <Typography variant='h2'>
-                    Way to go! Something cool awaits.
-                </Typography>
-                <Button>
-                    <Link href="/five">Next</Link>
-                </Button>
-            </Stack>
+            {phase == Phase.Story && story()}
+            {phase == Phase.Puzzle && puzzle()}
+            {phase == Phase.Completion && complete()}
         </Box>
-
-    }
     </div>
+
+    function story() {
+        return <div>
+            <Typography variant="body1">
+                Ok, finally, for real this time we’re on our way! <br />
+                But of course it&apos;s 2024 and the sleigh has autopilot. It basically flies itself!<br />
+                It's a long way down to Florida, and Santa needs to pass the time.<br />
+                He could use some help with his word search. Can you figure out the special word so Santa won&apos;t miss your stop?
+            </Typography>
+            <Button onClick={(_) => setPhase(Phase.Puzzle)}>
+                Sure!
+            </Button>
+        </div>
+    }
+
+    function puzzle() {
+        return <div>
+            <iframe src='https://strandstudio.thegoodboi.net/play?id=3FAR9S' height={700} />
+            <Button onClick={(_) => setPhase(Phase.Completion)}>I'm done!</Button>
+        </div>
+    }
+    function complete() {
+        return <div><Typography variant="body1">
+            What is the special word?
+        </Typography>
+            <TextField variant='outlined' label='Guess' onInput={(event: React.ChangeEvent<HTMLInputElement>) => {
+                if (event.target.value.toLowerCase() == "decorations") {
+                    setCorrect(true)
+                }
+            }} />
+            {isCorrect && <Button><Link href="/four">Down the chimney we go!</Link>
+            </Button>}
+        </div>
+    }
 }
