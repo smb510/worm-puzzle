ee2541990cd06a7c2735951df12ba04025a34ddf
Scottie Biddle <smb510@gmail.com>
2024-12-23 09:38:14 -0800

    format

--- app/two/page.tsx
+++ app/two/page.tsx
@@ -13,23 +13,23 @@ export default function PageTwo() {
     const [phase, setPhase] = useState(Phase.Story)
 
     return <div>
-    <OnCompleteHeader title="Chapter Two: Like Herding Cats..." isComplete={false} button={false} onComplete={(_: boolean) => { }}  />
-    <Box
+        <OnCompleteHeader title="Chapter Two: Like Herding Cats..." isComplete={false} button={false} onComplete={(_: boolean) => { }} />
+        <Box
             display="flex"
             justifyContent="center"
             alignItems="center">
-            {phase == Phase.Story && story() }
+            {phase == Phase.Story && story()}
             {phase == Phase.Puzzle && puzzle()}
             {phase == Phase.Completion && complete()}
         </Box>
-</div>
+    </div>
 
     function story() {
         return <div>
             <Typography variant="body1">
-            Phew! Now that Santa has unlocked his sleigh, he needs some help corralling his reindeer.<br />
-            He&apos;s got 6 of them there, Rudolph included, but 3 are still missing in action.<br />
-            Can you help Santa figure out which ones are missing?
+                Phew! Now that Santa has unlocked his sleigh, he needs some help corralling his reindeer.<br />
+                He&apos;s got 6 of them there, Rudolph included, but 3 are still missing in action.<br />
+                Can you help Santa figure out which ones are missing?
             </Typography>
             <Button onClick={(_) => setPhase(Phase.Puzzle)}>
                 Here, little reindeer!
